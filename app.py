c3399538c798f61f6c395de5624a16976b12320f
Iron Hoang <ironhoang@gmail.com>
2022-12-13 15:30:29 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+import time
+
+import redis
+from flask import Flask, request
+
+import websocket
+import _thread
+import time
+import rel
+
+def on_message(ws, message):
+    print(message)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws, close_status_code, close_msg):
+    print("### closed ###")
+
+def on_open(ws):
+    print("Opened connection")
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/wechat', methods=["POST"])
+def hello():
+    count = get_hit_count()
+    data = request.data
+    print(data, flush=True)
+    return 'Hello World! you have been seen {} times.\n'.format(count)
